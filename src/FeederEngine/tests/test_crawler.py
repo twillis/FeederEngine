4c9511e299b026da0b9c473c122c71d5ee6b82aa
Tom Willis <tom.willis@gmail.com>
2012-04-15 16:48:59 -0400

    back to 100% test coverage

--- src/FeederEngine/tests/test_crawler.py
+++ src/FeederEngine/tests/test_crawler.py
@@ -7,7 +7,7 @@ from webob import Request, Response
 import os
 from mimetypes import guess_type
 import md5
-from feederengine.crawler import crawl
+
 import logging
 
 logging.basicConfig(level="DEBUG")
@@ -53,20 +53,54 @@ def mock_rss_server(environ, start_response):
 
 class TestCrawler(unittest.TestCase):
     def testCrawlWorker(self):
+
+        from feederengine import crawler
+
         urls = ["http://www.reddit.com/r/Python/",
                 "http://slashdot.org",
                 "http://news.ycombinator.com/"]
         workers = {}
         for url in urls:
-            workers[url] = crawl(url=url)
+            workers[url] = crawler.crawl(url=url)
 
         results = [(k, str(w())) for k, w in workers.items()]
 
         print str(results)
 
     def testCrawlerFail(self):
+        from feederengine import crawler
+
         try:
-            str(crawl(url="http://1.1.1.1")(.01))
+            str(crawler.crawl(url="http://1.1.1.1")(.01))
             self.fail()
         except:
             pass
+
+    def testCrawlerFailGettingResponse(self):
+        """
+        strictly for test coverage
+        """
+        from feederengine import crawler
+
+        def err_app(environ, start_response):
+            raise Exception("fuuuuuuuu")
+        crawler.proxy = err_app
+        try:
+            str(crawler.crawl(url="http://1.1.1.1")(.01))
+            self.fail()
+        except:
+            pass
+
+    def testCrawlerCantCopyRequest(self):
+        """
+        strictly for test coverage
+        """
+        from feederengine import crawler
+        t = crawler._CrawlerThread(url="")
+        t._response = 1
+        try:
+            t.response
+            self.fail()
+        except:
+            pass
+
